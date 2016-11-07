f6c12cad8680d0bbe20bc7fc5550624a2a878ec8
Khai Chang <khai.chang89@gmail.com>
2016-11-07 17:31:18 -0600

    Feature - added tests (#1)
    * fix(.gitignore): removed the .idea directory
    
    * added some tests
    
    * added travis
    
    * added eslint
    
    * configured travis notifications
    
    * updated some its
    
    * passes tests
    
    * updated readme.md
    
    * updated readme.md again
    
    * added travis build badge

--- examples/express_test.js
+++ examples/express_test.js
@@ -3,68 +3,67 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-    Recaptcha = require('../lib/recaptcha').Recaptcha;
+var path = require('path');
+var express = require('express');
+var Recaptcha = require('../lib/recaptcha').Recaptcha;
 
-var PUBLIC_KEY  = 'YOUR_PUBLIC_KEY',
-    PRIVATE_KEY = 'YOUR_PRIVATE_KEY';
-
+var PUBLIC_KEY  = 'YOUR_PUBLIC_KEY';
+var PRIVATE_KEY = 'YOUR_PRIVATE_KEY';
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
+app.configure(function () {
+    app.set('views', path.join(__dirname, '/views'));
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions : true, showStack : true }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+app.configure('production', function () {
+    app.use(express.errorHandler());
 });
 
 // Routes
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY);
 
     res.render('form', {
-        layout: false,
-        locals: {
-            recaptcha_form: recaptcha.toHTML()
+        layout : false,
+        locals : {
+            recaptcha_form : recaptcha.toHTML()
         }
     });
 });
 
-app.post('/', function(req, res) {
+app.post('/', function (req, res) {
     var data = {
-        remoteip:  req.connection.remoteAddress,
-        response:  req.body['g-recaptcha-response']
+        remoteip : req.connection.remoteAddress,
+        response : req.body['g-recaptcha-response']
     };
     var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY, data);
 
-    recaptcha.verify(function(success, error_code) {
+    recaptcha.verify(function (success, error_code) {
         if (success) {
             res.send('Recaptcha response valid.');
-        }
-        else {
+        } else {
             // Redisplay the form.
             res.render('form', {
-                layout: false,
-                locals: {
-                    recaptcha_form: recaptcha.toHTML()
+                layout : false,
+                locals : {
+                    recaptcha_form : recaptcha.toHTML()
                 }
             });
         }
     });
 });
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(3000, function () {
+    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
